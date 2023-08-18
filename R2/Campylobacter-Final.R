ea268c12ab7a164b5a677c5ff6683d4adae27e69
Jinji Pang <pjj0702@kimki.student.iastate.edu>
2023-08-18 12:00:22 -0500

    rebuttal on 8-18-2023

--- R2/Campylobacter-Final.R
+++ R2/Campylobacter-Final.R
@@ -4,6 +4,7 @@ library(ggplot2)
 library(tidyverse)
 library(lme4)
 library(kableExtra)
+library(writexl)
 ##Campylobacter
 
 ##Step 1:Data cleaning
@@ -18,65 +19,143 @@ sapply(campydata, function(x) length(unique(x)))
 # FarmID     HouseID Sample_Type    sampleID  Farm_House diluation_1 diluation_3 diluation_4
 # 15           4           2          15         954         281         627         341
 # CFU        Test        Date       Cycle     BootDry       Flock
-# 984          71         109           8           2         410
+# 984          70         109           8           2         410
 
 
-#remove BB-H1,2,3,4, 2nd cycles
+# dfnosample<-subset(campydata,is.na(Test))
+
+#
+# dfBP<-subset(campydata,FarmID=="BP")
+# dfWL<-subset(campydata,FarmID=="WL")
+#
+# dfnosample%>%group_by(Sample_Type)%>%
+#   summarise(
+#     count = n()
+#   )
+
+#
+# write_xlsx(dfnosample, "../data/dfnosample.xlsx")
 
 
-campyda<-campydata[campydata$Flock != "BB - H1 - 2nd cycle" & campydata$Flock != "BB - H2 - 2nd cycle"&
-                     campydata$Flock != "BB - H3 - 2nd cycle" & campydata$Flock != "BB - H4 - 2nd cycle", ]
+campydata<-campydata%>%
+  mutate(
+    Campylobacter =case_when(
+      Test <= 0 ~ "Negative",
+      Test > 0 ~ "Positive"))
+
+
+# dfWL<-subset(campydata,FarmID=="WL")
+
+
+#remove no sample samples
+dftotal<-subset(campydata,!is.na(Test))
+
+dftotal%>%group_by(Sample_Type)%>%
+  summarise(
+    count = n()
+  )
+
+# Sample_Type count
+# <chr>       <int>
+#   B            1191
+#   C            6133
+
+
+sapply(dftotal, function(x) length(unique(x)))
+
+
+# > sapply(dftotal, function(x) length(unique(x)))
+# FarmID       HouseID   Sample_Type      sampleID    Farm_House   diluation_1   diluation_3
+# 15             4             2            15           954           279           626
+# diluation_4           CFU          Test          Date         Cycle       BootDry         Flock
+# 341           984            69           109             8             2           410
+# Campylobacter
+# 2
+
+
+
+#remove BB-H1,2,3,4, 2nd cycles,BP-H1,2,3,4, 8th cycles
+
+campyda<-dftotal[dftotal$Flock != "BB - H1 - 2nd cycle" & dftotal$Flock != "BB - H2 - 2nd cycle"&
+                   dftotal$Flock != "BB - H3 - 2nd cycle" & dftotal$Flock != "BB - H4 - 2nd cycle"&
+                 dftotal$Flock != "BP - H1 - 8th cycle" & dftotal$Flock != "BP - H2 - 8th cycle"&
+                   dftotal$Flock != "BP - H3 - 8th cycle" & dftotal$Flock != "BP - H4 - 8th cycle",]
+
+
+
 
 sapply(campyda, function(x) length(unique(x)))
 
-# FarmID     HouseID Sample_Type    sampleID  Farm_House diluation_1 diluation_3 diluation_4
-# 15           4           2          15         954         280         619         339
-# CFU        Test        Date       Cycle     BootDry       Flock
-# 984          71         109           8           2         406
+# > sapply(campyda, function(x) length(unique(x)))
+# FarmID       HouseID   Sample_Type      sampleID    Farm_House   diluation_1   diluation_3
+# 15             4             2            15           954           278           618
+# diluation_4           CFU          Test          Date         Cycle       BootDry         Flock
+# 339           984            69           108             8             2           402
+# Campylobacter
+# 2
+
+
 
 #remove boot sample dry
 DF<-subset(campyda,BootDry=="N")
 
 sapply(DF, function(x) length(unique(x)))
 
-# FarmID     HouseID Sample_Type    sampleID  Farm_House diluation_1 diluation_3 diluation_4
-# 15           4           2          15         954         258         615         332
-# CFU        Test        Date       Cycle     BootDry       Flock
-# 974          71         101           8           1         379
+# > sapply(DF, function(x) length(unique(x)))
+# FarmID       HouseID   Sample_Type      sampleID    Farm_House   diluation_1   diluation_3
+# 15             4             2            15           954           256           614
+# diluation_4           CFU          Test          Date         Cycle       BootDry         Flock
+# 332           974            69           100             8             1           375
+# Campylobacter
+# 2
 
+
+DF%>%group_by(Sample_Type)%>%
+  summarise(
+    count = n()
+  )
+
+# Sample_Type count
+# <chr>       <int>
+# 1 B          1110
+# 2 C         5608
+
+
+##Have a look at the samples with dry boot swabs, there are 27 flocks
 DFdry<-subset(campyda,BootDry=="Y")
 
 sapply(DFdry, function(x) length(unique(x)))
-# FarmID     HouseID Sample_Type    sampleID  Farm_House diluation_1 diluation_3 diluation_4
-# 8           4           2          15         485          56          23          22
-# CFU        Test        Date       Cycle     BootDry       Flock
-# 89          49           8           1           1          27
 
-#remove no sample samples
-df<-subset(DF,!is.na(Test))
+# > sapply(DFdry, function(x) length(unique(x)))
+# FarmID       HouseID   Sample_Type      sampleID    Farm_House   diluation_1   diluation_3
+# 8             4             2            15           486            56            23
+# diluation_4           CFU          Test          Date         Cycle       BootDry         Flock
+# 22            89            49             8             1             1            27
+# Campylobacter
+# 2
 
-#campydf has 6779 samples
 
-sapply(df, function(x) length(unique(x)))
+#DF has 6778 samples
 
-# FarmID     HouseID Sample_Type    sampleID  Farm_House diluation_1 diluation_3 diluation_4
-# 15           4           2          15         954         256         614         332
-# CFU        Test        Date       Cycle     BootDry       Flock
-# 974          70         101           8           1         379
+sapply(DF, function(x) length(unique(x)))
+
 
 # A visual way to check for missing data
-missmap(df, main = "Missing values vs observed")
+missmap(DF, main = "Missing values vs observed")
 
 # Subsetting the data
-data <- subset(df,select=c(1,2,3,10,11,12,13,14))
+data <- subset(DF,select=c(1,2,3,10,11,12,13,14,15))
 
 missmap(data, main = "Missing values vs observed")
 
 ## check levels of each variable
 sapply(data, function(x) length(unique(x)))
 
-# FarmID     HouseID Sample_Type        Test        Date       Cycle     BootDry       Flock
-# 15           4           2          70         101           8           1         379
+# > sapply(data, function(x) length(unique(x)))
+# FarmID       HouseID   Sample_Type          Test          Date         Cycle       BootDry
+# 15             4             2            69           100             8             1
+# Flock Campylobacter
+# 375             2
 
 ##change variable attribute
 data$FarmID <- as.factor(data$FarmID)
@@ -86,18 +165,10 @@ data$Test<-as.numeric(data$Test)
 data$Date<-as.Date(data$Date, "%m/%d/%y")
 data$Cycle <- as.factor(data$Cycle)
 data$BootDry <- as.factor(data$BootDry)
-
+data$Flock<-as.factor(data$Flock)
+data$Campylobacter<-as.factor(data$Campylobacter)
 
 str(data)
-
-##Mutate new variables
-##1.create Campylobacter variable based on CFU
-data<-data%>%
-  mutate(
-    Campylobacter =case_when(
-      Test <= 0 ~ "Negative",
-      Test > 0 ~ "Positive"
-    ))
 
 ##use number to show campy status, create target variable, target=1, positive;target=0,negative
 data$target <- 0
@@ -129,8 +200,9 @@ data<-data%>%
 
 data$season <- as.factor(data$season)
 
+unique(data$season)
 
-
+data$season<-factor(data$season, levels = c("Spring","Summer","Fall","Winter"))
 
 data%>%group_by(Sample_Type)%>%
   summarise(
@@ -139,7 +211,8 @@ data%>%group_by(Sample_Type)%>%
 
 # Sample_Type    count
 #   B            1110
-#   C            5668
+#   C            5608
+
 
 
 ## Step 1 univariate analysis, did this but turned out all the predictors had significant effect on the Campylobacter
@@ -178,6 +251,64 @@ se <- sqrt(diag(vcov(model_cycle)))
 # table of estimates with 95% CI
 tab <- cbind(Est = fixef(model_cycle), LL = fixef(model_cycle) - 1.96 * se, UL = fixef(model_cycle) + 1.96 *
                 se)
+
+print(exp(tab))
+
+###########
+
+# Fit a logistic mixed-effects model, with month
+
+model_month <- glmer(target ~ Sample_Type + Month + (1 | FarmID) + (1 | FarmID:HouseID) + (1 | FarmID:HouseID:Cycle),
+                     data = data, family = binomial)
+
+
+# View the model summary
+summary(model_month)
+
+
+vcov(model_month)
+
+se <- sqrt(diag(vcov(model_month)))
+# table of estimates with 95% CI
+tab <- cbind(Est = fixef(model_month), LL = fixef(model_month) - 1.96 * se, UL = fixef(model_month) + 1.96 *
+               se)
+
+print(exp(tab))
+
+##08-18-2023, minor revision for avian disease paper
+# > print(exp(tab))
+# Est           LL           UL
+# (Intercept)    3.324981e-05 1.033185e-07 1.070041e-02
+# Sample_TypeC   2.430417e+01 1.642097e+01 3.597186e+01
+# Cycle2nd cycle 2.564350e+03 1.861002e+01 3.533522e+05
+# Cycle3rd cycle 2.461029e+00 1.910608e-02 3.170018e+02
+# Cycle4th cycle 1.852431e+01 1.306907e-01 2.625665e+03
+# Cycle5th cycle 1.142146e-01 1.306393e-03 9.985487e+00
+# Cycle6th cycle 2.905455e-02 3.067735e-04 2.751759e+00
+# Cycle7th cycle 2.187093e+02 1.591126e+00 3.006283e+04
+# Cycle8th cycle 2.607471e+04 9.064713e+01 7.500408e+06
+
+
+
+# Fit a logistic mixed-effects model, with month
+
+
+
+
+model_season <- glmer(target ~ Sample_Type + season + (1 | FarmID) + (1 | FarmID:HouseID) + (1 | FarmID:HouseID:Cycle),
+                     data = data, family = binomial)
+
+
+# View the model summary
+summary(model_season)
+
+
+vcov(model_season)
+
+se <- sqrt(diag(vcov(model_season)))
+# table of estimates with 95% CI
+tab <- cbind(Est = fixef(model_season), LL = fixef(model_season) - 1.96 * se, UL = fixef(model_season) + 1.96 *
+               se)
 
 print(exp(tab))
 
